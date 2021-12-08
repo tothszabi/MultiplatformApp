8a4646e16852f1674b59f6f1fb87d1d63a3fa617
Szabolcs Toth <szabolcs.toth@bitrise.io>
2021-12-08 16:24:38 +0000

    Initial Commit

---
+++ Shared/Multiplatform_appApp.swift
@@ -0,0 +1,17 @@
+//
+//  Multiplatform_appApp.swift
+//  Shared
+//
+//  Created by Szabolcs Toth on 08/12/2021.
+//
+
+import SwiftUI
+
+@main
+struct Multiplatform_appApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
